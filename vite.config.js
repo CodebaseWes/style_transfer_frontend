ce6aee84757a62c012a7f4eff252b9d1cd3c9e1a
Wes <wfarmer2718@protonmail.com>
2022-11-12 19:48:47 -0500

    Remove has in asset names

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }, 
+  build: {
+    rollupOptions: {
+        input:{
+            main: "index.html",
+        },
+        output: {
+            // Prevent vendor.js being created
+            manualChunks: undefined,
+            // chunkFileNames: "zzz-[name].js",
+            // this got rid of the hash on style.css
+            assetFileNames: "assets/[name].[ext]",
+            entryFileNames: "assets/[name].js",
+        },
+    },
   }
 })
